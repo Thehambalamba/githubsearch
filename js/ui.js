fba63d1c387a1103c3e158a2a6309db7ad462323
Nikola Pervic <nikolapervic@me.com>
2018-02-13 17:34:43 +0100

    Alerts
    Added functions that will show/hide an alert when we get a 404 from our promise.

--- js/ui.js
+++ js/ui.js
@@ -7,8 +7,8 @@ class UI {
         this.profile.innerHTML = `
             <div class="card card-body mb-3">
                 <div class="row">
-                    <div class="col-md-6">
-                        <img clas="img-fluid mb-2" src="${user.avatar_url}">
+                    <div class="col-md-6 text-center">
+                        <img class="img-fluid mb-2" src="${user.avatar_url}">
                         <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4 mt-4">View Profile</a>
                     </div>
                     <div class="col-md-6">
@@ -29,5 +29,37 @@ class UI {
             <h3 class="page-heading mb-3">Latest Repos</h3>
             <div id="repos"></div>
         `;
-    } 
+    }
+
+    
+    showAlert(message, className) {
+        this.clearAlert(); // Clear the alerts first
+
+        const div = document.createElement('div'); // Div to show message
+        const container = document.querySelector('.searchContainer');
+        const search = document.querySelector('.search');
+
+        // Set class
+        div.className = className;
+
+        // Add mesage
+        div.appendChild(document.createTextNode(message));
+        container.insertBefore(div, search);
+
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
 }
